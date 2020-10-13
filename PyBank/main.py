4093a8241fca93fcf57cfc625ecac29f855da6d0
olivedisaster <buckley.weglarz@gmail.com>
2020-10-12 18:32:45 -0700

    3rd commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,80 @@
-test
+#Modules
+import os
+import csv
+
+budget = "Desktop/Bootcamp/Homework/HW 2/python-challenge/PyBank/Resources/budget_data.csv"
+
+csvpath = os.path.join("Resources","budget_data.csv")
+
+#Define Variables
+months_tot = 0
+profit_tot = 0
+profit_chg_tot = 0
+chg_tot = 0
+pl_delta = 0
+pl_old_month = 0
+pl_new_month = 0
+pl_mean = []
+gr_inc = 0
+gr_dec = 99999999999
+gr_month_inc = ""
+gr_month_dec = ""
+firsttime = True
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        #Adding total months
+        months_tot = months_tot + 1
+        
+        #Adding and printing total profits
+        profit_tot = profit_tot + int(row[1])
+        
+        #Average profit/losses
+        if not firsttime:
+            profit_chg = int(row[1]) - previous_value
+            profit_chg_tot += profit_chg
+
+            if profit_chg > gr_inc:
+                gr_inc = profit_chg
+                gr_month_inc = row[0]
+
+            if profit_chg < gr_dec:
+                gr_dec = profit_chg
+                gr_month_dec = row[0]
+            
+
+        firsttime = False
+        previous_value = int(row[1])
+
+    profit_chg_tot = profit_chg_tot/(months_tot - 1)
+    profit_chg_tot = round(profit_chg_tot, 2)
+
+    print(f"Total Months: {(months_tot)}")
+    print(f"Total: {(profit_tot)}")
+    print(f"Average Change: ${profit_chg_tot}")
+    print(f"Greatest Increase in Profits: {gr_month_inc} ${gr_inc}")
+    print(f"Greatest Decrease in Profits: {gr_month_dec} ${gr_dec}")
+
+output_path = os.path.join("analysis", "Financial_Analysis.txt")
+
+f = open(output_path, 'w')
+
+f.write("Financial Analysis\n")
+f.write("_______________________________\n")
+f.write(f"Total Months: {(months_tot)}\n")
+f.write(f"Total: ${profit_tot}\n")
+f.write(f"Total Change: ${profit_chg_tot}\n")
+f.write(f"Greatest Increase in Profits: {gr_month_inc} (${gr_inc})\n")
+f.write(f"Greatest Decrease in Profits: {gr_month_dec} (${gr_dec})\n")
+    
+
+  
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,68 @@
-test
+#Modules
+import os
+import csv
+
+election = "Desktop/Bootcamp/Homework/HW 2/python-challenge/PyPoll/Resources/election_data.csv"
+
+#Set file path
+csvpath = os.path.join("Resources","election_data.csv")
+
+#Define Variables
+votes_tot = 0
+cand_rec_vote = 0
+percent_vote = 0
+cand_vot_tot = 0
+#winner = []
+
+#Set empty dictionary
+candidates = {}
+
+#Open CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader:
+        votes_tot = votes_tot + 1
+        candidate = row[2]
+
+        if candidate not in candidates:
+            candidates.update({candidate : 1})
+        
+        else:
+            candidates[candidate] += 1
+
+    print(candidates)
+
+print("Election Results")
+print("__________________________")
+print(f"Total Votes: {votes_tot}")
+print("__________________________")
+
+winner_name = ""
+winner = 0
+
+for candidate in candidates:
+    print("{candidate}: {candidates[candidate]/votes_tot)*100:.3f}% ({candidates[candidate]})")
+    if candidates[candidate] > winner:
+        winner = candidates[candidate]
+        winner_name = candidate
+print("_________________________")
+print(f"Winner: {winner_name}")
+output_path = os.path.join("analysis", "Election_Results_Analysis.txt")
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter = ',')
+    csvwriter.writerow(["Election Results"])
+    csvwriter.writerow(['-----------------------------'])
+    csvwriter.writerow([f"Total Votes: {votes_tot}"])
+    csvwriter.writerow(['-----------------------------'])
+    for candidate in candidates:
+        csvfile.write(f"{candidate}: {(candidates[candidate]/votes_tot)*100:.3f}% ({candidates[candidate]})\n")
+    csvfile.write("---------------------------\n")
+    csvfile.write(f"Winner: {winner_name}\n")
+    csvfile.write("---------------------------\n")
+  
+
+
